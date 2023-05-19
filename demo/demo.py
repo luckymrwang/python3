7362c4c5cf8b38b57c8f64e2f66e544a71f39fe0
Cookie Wang <luckymrwang@163.com>
2023-05-19 09:42:43 +0800

    update

---
+++ demo/demo.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+print("start....")
+x=2
+y=""
+if x==2:
+    y=1
+print(y)
--- trains/test.py
+++ trains/test.py
@@ -1,3 +1,18 @@
-a = 'abc '
-print(a.capitalize())
-print(a)
+import time
+import os
+
+# 创建子进程前声明的变量
+number = 7
+try:
+    pid = os.fork()
+
+    if pid == 0:
+        print("this is child process")
+        number = number - 1
+        time.sleep(5)
+        print(number)
+    else:
+        print(number)
+        print("this is parent process")
+except OSError:
+    pass
